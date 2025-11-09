a2b0c7f0a4e62338c0b4cfc8f65f5aacdefa0ef0
rrrrayyyy <john.wick.29gjslug9240g@gmail.com>
2025-11-09 19:41:42 +0900

    refactoring proto

--- payment-service/src/main/java/com/example/payment_service/adapter/KafkaProtobufSerializer.java
+++ payment-service/src/main/java/com/example/payment_service/adapter/KafkaProtobufSerializer.java
@@ -2,12 +2,11 @@ package com.example.payment_service.adapter;
 
 import org.apache.kafka.common.serialization.Serializer;
 
-import com.example.proto.ProtobufSerializer;
 import com.google.protobuf.Message;
 
 public class KafkaProtobufSerializer<T extends Message> implements Serializer<T> {
 	@Override
 	public byte[] serialize(String topic, T data) {
-		return ProtobufSerializer.serialize(data);
+		return data == null ? null : data.toByteArray();
 	}
 }
--- proto/src/main/java/com/example/proto/ProtobufSerializer.java
+++
@@ -1,9 +0,0 @@
-package com.example.proto;
-
-import com.google.protobuf.Message;
-
-public class ProtobufSerializer {
-    public static byte[] serialize(Message message) {
-        return message == null ? null : message.toByteArray();
-    }
-}
