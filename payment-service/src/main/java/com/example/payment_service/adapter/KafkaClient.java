457ab3f4d0f9ffe8ca8274da6dde01d45d2f49b9
rrrrayyyy <john.wick.29gjslug9240g@gmail.com>
2025-11-21 18:37:39 +0900

    refactorings

--- payment-service/src/main/java/com/example/payment_service/adapter/KafkaClient.java
+++ payment-service/src/main/java/com/example/payment_service/adapter/KafkaClient.java
@@ -9,13 +9,15 @@ import com.example.payment_service.usecase.PaymentEventProducer;
 @Service
 public class KafkaClient implements PaymentEventProducer {
 	private final KafkaTemplate<String, PaymentEventValue> protoTemplate;
+	private final Topic paymentTopic;
 
-	public KafkaClient(KafkaTemplate<String, PaymentEventValue> protoTemplate) {
+	public KafkaClient(KafkaTemplate<String, PaymentEventValue> protoTemplate, KafkaTopicConfig topicConfig) {
 		this.protoTemplate = protoTemplate;
+		paymentTopic = topicConfig.getTopics().get("payment");
 	}
 
 	@Override
 	public void publish(PaymentEventValue value) {
-		protoTemplate.send("payment-events", value);
+		protoTemplate.send(paymentTopic.getName(), value);
 	}
 }
